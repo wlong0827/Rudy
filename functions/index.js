d4ba2e644c0ce19188060a3007eebc1a9abc2961
William Long <wlong0827@gmail.com>
2018-10-05 21:27:16 -0500

    working update

--- functions/index.js
+++ functions/index.js
@@ -94,7 +94,6 @@ function printMessage(activity) {
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
@@ -112,58 +111,34 @@ exports.receivedQuery = functions.database.ref('/conversations/-L-EhR3Ps0OXpU6hv
 		return console.log("Bot already responded");
 	}
 
-	if(message.indexOf("status") != -1) {
-		curBotResponse = "The Agate 9H well completion is proceeding normally. \n1. Treating Pressure is at 4.3 psi (+16.3%) \n2. Friction Reducer output is at 1.5 (+8.2%) \n3. Sand consumption is at 243 kg/min (-4.2%) \n4. Water consumption is at 3,304 gal/min (+7.5%) \n5. Slurry rate is at 0.107 bbl/min (-12.3%)"
-	}
-	else if(message.indexOf("far away") != -1) {
-		curBotResponse = "The USFleet LM1022 hauler is 46 miles away. Would you like to call the driver?";
-		time2 = time + 1;
-		const response1 = {
-			content : curBotResponse,
-			fromID : "1vj712KhmheBF6sPdXY9wdOfKyj1",
-			toID : "sNVP4BfozOZhmeXk9KVIhkpuGq52",
-			isRead : false,
-			type : "text",
-			timestamp : time
-		};
-		const response2 = {
-			content : "35.2552909944809:-97.5015119968978",
-			fromID : "1vj712KhmheBF6sPdXY9wdOfKyj1",
-			toID : "sNVP4BfozOZhmeXk9KVIhkpuGq52",
-			isRead : false,
-			type : "location",
-			timestamp : time2
-		};
+	// once the client is ready, create a new conversation 
+	directLineClient.then(function (client) {
+    	sendMessageFromConsole(client, curConversationId, curBotResponse);
+    });
 
-		return event.data.ref.parent.child('Bot Response - '.concat(time.toString())).set(response1).then(function() {
-			event.data.ref.parent.child('Bot Response - '.concat(time2.toString())).set(response2)
-		});
-	} else if(message.indexOf("Show me") != -1) {
-		curBotResponse = "Here's Treating Pressure over the last hour:";
-		time2 = time + 1;
-		const response1 = {
-			content : curBotResponse,
-			fromID : "1vj712KhmheBF6sPdXY9wdOfKyj1",
-			toID : "sNVP4BfozOZhmeXk9KVIhkpuGq52",
-			isRead : false,
-			type : "text",
-			timestamp : time
-		};
-		const response2 = {
-			content : "https://firebasestorage.googleapis.com/v0/b/rudy-881bb.appspot.com/o/messagePics%2Fgraph.png?alt=media&token=6f683f5a-cf53-4aa3-a852-7850f9bb2e36",
-			fromID : "1vj712KhmheBF6sPdXY9wdOfKyj1",
-			toID : "sNVP4BfozOZhmeXk9KVIhkpuGq52",
-			isRead : false,
-			type : "photo",
-			timestamp : time2
-		};
-		return event.data.ref.parent.child('Bot Response - '.concat(time.toString())).set(response1).then(function() {
-			event.data.ref.parent.child('Bot Response - '.concat(time2.toString())).set(response2)
-		});
-	} else if(message.indexOf("No thanks") != -1) {
-		curBotResponse = "Ok";
-	} else {
-		curBotResponse = "I'm sorry. I couldn't understand your query.";
+    const response = {
+		content : curBotResponse,
+		fromID : "1vj712KhmheBF6sPdXY9wdOfKyj1",
+		toID : "sNVP4BfozOZhmeXk9KVIhkpuGq52",
+		isRead : false,
+		type : "text",
+		timestamp : time
+	};
+
+	return event.data.ref.parent.child('Bot Response - '.concat(time.toString())).set(response);
+});
+
+exports.receivedQuery2 = functions.database.ref('/conversations/-L-KIgfpDygBldHEKtZa/{id}').onCreate(event => {
+
+	const messageId = event.params.id;
+	const message = event.data.val().content;
+	const time = Math.floor(Date.now() / 1000);
+
+	console.log("Message ID is ", messageId);
+	console.log("Message is ", message);
+
+	if(messageId.indexOf("Bot Response") != -1) {
+		return console.log("Bot already responded");
 	}
 
 	// once the client is ready, create a new conversation 
@@ -174,7 +149,7 @@ exports.receivedQuery = functions.database.ref('/conversations/-L-EhR3Ps0OXpU6hv
     const response = {
 		content : curBotResponse,
 		fromID : "1vj712KhmheBF6sPdXY9wdOfKyj1",
-		toID : "sNVP4BfozOZhmeXk9KVIhkpuGq52",
+		toID : "ySqfyMvNzoXw39b4bXcjy5xcqlj2",
 		isRead : false,
 		type : "text",
 		timestamp : time
